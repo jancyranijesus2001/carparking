c4fa863ada0926c4ba526db3ee3b8e074de41b99
jancyranijesus2001 <115150542+jancyranijesus2001@users.noreply.github.com>
2025-09-06 12:37:04 +0530

    Update settings.py

--- carparking/settings.py
+++ carparking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ke9y8snqw(0=p(it7ua4klbozv7dsq^zp2_i01iz_$hcj(4q02
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,3 +124,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+

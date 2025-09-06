4d4e985b6ae325774a4bf33ca383d5501751d081
jancyranijesus2001 <115150542+jancyranijesus2001@users.noreply.github.com>
2025-09-06 11:39:01 +0530

    Delete app directory

--- app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(SmokeData)
-admin.site.register(HumanData)
-admin.site.register(ObjectData)
